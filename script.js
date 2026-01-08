140ffa6e2d676bbd48ec8004884a10d09787f2a7
bakyybroklim-hash <bakyybroklim@gmail.com>
2026-01-08 12:46:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let html_p= document.getElementById('ghj1');
+
+
+html_p.addEventListener('click', ()=>{
+   html_p.style.color='red';
+   html_p.style.fontSize='50px';
+   html_p.style.backgroundColor='yellow';
+   html_p.style.textAlign='center';
+   html_p.style.border='2px solid black';
+})
+
